1910f43b8cf92b562a26d33f08e5cefca67d0754
unknown <parmarparth954@gmail.com>
2024-08-24 09:49:03 +0530

    code

---
+++ main.js
@@ -0,0 +1,21 @@
+import GeoTIFF from 'ol/source/GeoTIFF.js';
+import Map from 'ol/Map.js';
+import TileLayer from 'ol/layer/WebGLTile.js';
+
+const source = new GeoTIFF({
+  sources: [
+    {
+      url: "./cog/ndvi.tif",
+    },
+  ],
+});
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: source,
+    }),
+  ],
+  view: source.getView(),
+});
